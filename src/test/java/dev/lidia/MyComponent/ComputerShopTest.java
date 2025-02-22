a8a7bcb6f6a696147513571e8b33fa1e2d131e38
Lidia Glez-Tablas <lgtablasf5@gmail.com>
2025-02-22 21:44:56 +0100

    test the arrayList attribute of the ComputerShop class, test fault

--- src/test/java/dev/lidia/MyComponent/ComputerShopTest.java
+++ src/test/java/dev/lidia/MyComponent/ComputerShopTest.java
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
+import dev.lidia.MyComponent.Computer;
+
+import java.util.List;
+
 
 
 
@@ -21,8 +25,21 @@ public class ComputerShopTest {
         assertThat (tienda.getNombre(), is("Tienda1"));
         assertThat (tienda.getPropietario(), is("Jhon Doe"));
         assertThat (tienda.getIdTributario(), is(123456789));
-
     }
 
+    @Test
+    @DisplayName ("testamos atributo array de la clase ComputerShop") 
 
+    public void testGetterListComputadoras() {
+        ComputerShop tienda = new ComputerShop("Tienda1", "Jhon Doe", 123456789);
+        Computer computadora1 = new Computer("HP", "Pavilion", 8, 500, 1000);
+        Computer computadora2 = new Computer("Lenovo", "Thinkpad", 16, 1000, 1500);
+        tienda.addComputer(computadora1);
+        tienda.addComputer(computadora2);
+
+        List <Computer> computadoras = tienda.getComputadoras();
+        assertThat (computadoras.size(), is(2));
+        assertThat (computadoras, hasItems(computadora1, computadora2));
+
+    }
 }
