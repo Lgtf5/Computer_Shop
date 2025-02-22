276589971711b00e14e960d82892050df75119a4
Lidia Glez-Tablas <lgtablasf5@gmail.com>
2025-02-22 21:33:07 +0100

    test the simple attributes of the ComputerShop class, test succesfull

--- src/main/java/dev/lidia/MyComponent/ComputerShop.java
+++ src/main/java/dev/lidia/MyComponent/ComputerShop.java
@@ -1,6 +1,19 @@
 package dev.lidia.MyComponent;
 
 
+
+
 public class ComputerShop {
-    
+    private String nombre;
+    private String propietario;
+    private int idTributario;
+
+    public ComputerShop(String nombre, String propietario, int idTributario) {
+        this.nombre = nombre;
+        this.propietario = propietario;
+        this.idTributario = idTributario;
+    }
+    public String getNombre() { return nombre;}
+    public String getPropietario() { return propietario;}
+    public int getIdTributario() { return idTributario;}
 }
--- src/test/java/dev/lidia/MyComponent/ComputerShopTest.java
+++ src/test/java/dev/lidia/MyComponent/ComputerShopTest.java
@@ -3,7 +3,11 @@ package dev.lidia.MyComponent;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.is;
+
+
+
+
 
 public class ComputerShopTest {
 
@@ -12,11 +16,12 @@ public class ComputerShopTest {
     @DisplayName ("testamos atributos de la clase ComputerShop")
 
     public void testGettersAtributosTienda() {
-        ComputerShop tienda = new ComputerShop("Tienda1", "Jhon Doe", "123456789");
+        ComputerShop tienda = new ComputerShop("Tienda1", "Jhon Doe", 123456789);
 
         assertThat (tienda.getNombre(), is("Tienda1"));
         assertThat (tienda.getPropietario(), is("Jhon Doe"));
-        assertThat (tienda.getIdTributario(), is("123456789"));
+        assertThat (tienda.getIdTributario(), is(123456789));
+
     }
 
 
