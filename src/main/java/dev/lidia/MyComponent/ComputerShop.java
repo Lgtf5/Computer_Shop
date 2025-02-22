86e32c7477608949f6ea50f7f68015911e4102de
Lidia Glez-Tablas <lgtablasf5@gmail.com>
2025-02-22 23:29:33 +0100

    test the arrayList attribute of the ComputerShop class, test succesfull

--- src/main/java/dev/lidia/MyComponent/ComputerShop.java
+++ src/main/java/dev/lidia/MyComponent/ComputerShop.java
@@ -1,19 +1,29 @@
 package dev.lidia.MyComponent;
 
-
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class ComputerShop {
     private String nombre;
     private String propietario;
     private int idTributario;
+    private List<Computer> computadoras;
+
 
     public ComputerShop(String nombre, String propietario, int idTributario) {
         this.nombre = nombre;
         this.propietario = propietario;
         this.idTributario = idTributario;
+        this.computadoras = new ArrayList<>();
+
     }
+
+    public void addComputer(Computer computadora) {
+        this.computadoras.add(computadora);
+    }
+
     public String getNombre() { return nombre;}
     public String getPropietario() { return propietario;}
     public int getIdTributario() { return idTributario;}
+    public List<Computer> getComputadoras() { return computadoras;}
 }
--- src/test/java/dev/lidia/MyComponent/ComputerShopTest.java
+++ src/test/java/dev/lidia/MyComponent/ComputerShopTest.java
@@ -1,20 +1,16 @@
 package dev.lidia.MyComponent;
+import dev.lidia.MyComponent.Computer;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItems;
 import static org.hamcrest.Matchers.is;
-
-import dev.lidia.MyComponent.Computer;
-
 import java.util.List;
 
 
 
-
-
 public class ComputerShopTest {
-
 
     @Test
     @DisplayName ("testamos atributos de la clase ComputerShop")
@@ -27,13 +23,15 @@ public class ComputerShopTest {
         assertThat (tienda.getIdTributario(), is(123456789));
     }
 
+
     @Test
     @DisplayName ("testamos atributo array de la clase ComputerShop") 
 
     public void testGetterListComputadoras() {
-        ComputerShop tienda = new ComputerShop("Tienda1", "Jhon Doe", 123456789);
-        Computer computadora1 = new Computer("HP", "Pavilion", 8, 500, 1000);
-        Computer computadora2 = new Computer("Lenovo", "Thinkpad", 16, 1000, 1500);
+        ComputerShop tienda = new ComputerShop ("Tienda1", "Jhon Doe", 123456789);
+        Computer computadora1 = new Computer();
+        Computer computadora2 = new Computer();
+
         tienda.addComputer(computadora1);
         tienda.addComputer(computadora2);
 
