c7d8fba1d2cfc8edfe5c3a9b8dc7e1df340f2e8f
Lidia Glez-Tablas <lgtablasf5@gmail.com>
2025-02-22 22:10:55 +0100

    tested the attributes of the Computer class, test succesfull

--- src/main/java/dev/lidia/MyComponent/Computer.java
+++ src/main/java/dev/lidia/MyComponent/Computer.java
@@ -2,5 +2,23 @@ package dev.lidia.MyComponent;
 
 
 public class Computer {
+    private String marca;
+    private int memoria;
+    private String procesador;
+    private String sistemaOperativo;
+    private double precio;
     
+    public Computer(String marca, int memoria, String procesador, String sistemaOperativo, double precio) {
+        this.marca = marca;
+        this.memoria = memoria;
+        this.procesador = procesador;
+        this.sistemaOperativo = sistemaOperativo;
+        this.precio = precio;
+    }
+
+    public String getMarca() { return marca;}
+    public int getMemoria() { return memoria;}
+    public String getProcesador() { return procesador;}
+    public String getSistemaOperativo() { return sistemaOperativo;}
+    public double getPrecio() { return precio;}
 }
