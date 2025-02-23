69168987008aa3d8e2b799486c9b4fa38aabf2d4
Lidia Glez-Tablas <lgtablasf5@gmail.com>
2025-02-23 17:06:54 +0100

    tested attributes to FlowStock class, test succesfull

--- src/main/java/dev/lidia/MyComponent/FlowStock.java
+++ src/main/java/dev/lidia/MyComponent/FlowStock.java
@@ -1,5 +1,27 @@
 package dev.lidia.MyComponent;
 
+import java.time.LocalDate;
+
+
 public class FlowStock {
     
+    public String tipo; 
+    public String marca;
+    public int unidadesStock;
+    public LocalDate fechaStock;
+    public String detalle;
+
+    public FlowStock(String tipo, String marca, int unidadesStock, LocalDate fechaStock, String detalle) {
+        this.tipo = tipo;
+        this.marca = marca;
+        this.unidadesStock = unidadesStock;
+        this.fechaStock = LocalDate.now();
+        this.detalle = detalle;
+    }
+
+    public String getTipo() { return tipo;}
+    public String getMarca() { return marca;}
+    public int getUnidadesStock() { return unidadesStock;}  
+    public LocalDate getFechaStock() { return LocalDate.now();}
+    public String getDetalle() { return detalle;}
 }
--- src/test/java/dev/lidia/MyComponent/FlowStockTest.java
+++ src/test/java/dev/lidia/MyComponent/FlowStockTest.java
@@ -1,38 +1,42 @@
 package dev.lidia.MyComponent;
 
 import java.sql.Date;
-import java.util.concurrent.Flow;
+
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-
+import static org.hamcrest.Matchers.notNullValue;
+import java.time.LocalDate; 
 
 public class FlowStockTest {
     private FlowStock movimiento;
 
     @Before
     public void setup() {
-        movimiento = new FlowStock("ENTRADA", "HP", 50, new Date(), "Compra de 50 HP para lanzamiento tienda");
+        movimiento = new FlowStock("ENTRADA", "HP", 50, LocalDate.now(), "Compra de 50 HP para lanzamiento tienda");
     }
 
     @Test
     @DisplayName ("tested attributes to FlowStock class")
 
-    public void testFlowStock() {
+    public void FlowStock() {
         
-        private String tipo;
-        private String marca;
-        private int unidadesStock;
-        private Date fechaStock;
-        private String detalle;
+        String tipo;
+        String marca;
+        int unidadesStock;
+        Date fechaStock;
+        String detalle;
+    
 
         assertThat (movimiento.getTipo(), is("ENTRADA"));
         assertThat (movimiento.getMarca(), is("HP"));
         assertThat (movimiento.getUnidadesStock(), is(50));
-        assertThat (movimiento.getFechaStock(), is(new Date()));
+        assertThat (movimiento.getFechaStock(), is(notNullValue()));
         assertThat (movimiento.getDetalle(), is("Compra de 50 HP para lanzamiento tienda"));
+    
     }
+
 }
